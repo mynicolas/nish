346583bc887bf60a457412e5fcec7934d74459bf
Nicolas-Mac <nicolas@nicolas-mac.local>
2017-01-17 20:03:52 +0800

    v0.10

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cp ./nish /usr/local/bin/
 cp ./nish-db /usr/local/bin/
+cp ./config ~/.ssh/
+if [ ! -d ~/.ssh/socks ]; then
+    mkdir ~/.ssh/socks
+fi
 if [ ! -d ~/ssh-server ]; then
     mkdir ~/ssh-server
 fi
